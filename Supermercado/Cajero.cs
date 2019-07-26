b94ed9e7831d7c0a23234c71ab0e74ae25191e84
Panda-G <neo.junior.16@gmail.com>
2019-07-25 23:35:02 -0300

    Creando primera version

---
+++ Supermercado/Cajero.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+
+
+namespace Supermercado
+{
+	public class Cajero : Persona
+	{
+		private string horario= null;
+		private int CodigoCajero = 0;
+
+		public string getHorario(){
+			return this.horario;
+		}
+
+		public void setHorario(string nuevoHorario){
+			this.horario = nuevoHorario;
+		}
+
+		public int getCodigoCajero(){
+			return this.CodigoCajero;
+		}
+
+		public void setCodigoCajero(int nuevoCodigoCajero){
+			this.CodigoCajero = nuevoCodigoCajero;
+		}
+
+		public string mostrarCajero(){
+			return "Nombre: " + this.getNombre () + " Apellido: " + this.getApellido ()
+			+ " DNI: " + this.getDni().ToString( ) + " Código del cajero: " + this.getCodigoCajero ()
+			+ " Horario: " + this.getHorario () ;
+		}
+
+
+	}
+}
+
+
+
+
---
+++ Supermercado/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+
+namespace Supermercado
+{
+	public class MainClass
+	{
+		public static void Main (string[] args)
+		{	
+			ArrayList listaProductos = new ArrayList();
+			ArrayList listaPromociones = new ArrayList ();
+			ArrayList listaCajas = new ArrayList();
+			ArrayList listaCajeros = new ArrayList ();
+
+			Supermercado super = new Supermercado (listaProductos, listaPromociones, listaCajas, listaCajeros);
+			super.iniciar ();
+		}
+	}
+}
